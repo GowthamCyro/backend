0e4154f7364a70d78f67dfc01772aceed533171a
GowthamCyro <cyrogamer.yt@gmail.com>
2024-08-02 12:59:27 +0530

    Untested - RegisterController

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -2,6 +2,8 @@ import { v2 as cloudinary } from 'cloudinary';
 import exp from 'constants';
 import fs from "fs";
 
+
+
 cloudinary.config({ 
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
     api_key: process.env.CLOUDINARY_API_KEY, 
